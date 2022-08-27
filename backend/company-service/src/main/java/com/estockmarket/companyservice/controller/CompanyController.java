c9bf88d3d80f51d89a01da6659069b7da14d9c80
Abdus Saboor <gaffari98@gmail.com>
2022-08-27 18:48:25 +0530

    added openapi and swagger

--- backend/company-service/src/main/java/com/estockmarket/companyservice/controller/CompanyController.java
+++ backend/company-service/src/main/java/com/estockmarket/companyservice/controller/CompanyController.java
@@ -1,10 +1,24 @@
 package com.estockmarket.companyservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.estockmarket.companyservice.dto.CompanyDTO;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/v1.0/market/company/")
 public class CompanyController {
+    
+    @PostMapping(value="test")
+    public ResponseEntity<CompanyDTO> postMethodName(@RequestBody CompanyDTO model) {
+
+        return new ResponseEntity<>(model, HttpStatus.CREATED);
+    }
     
 }
