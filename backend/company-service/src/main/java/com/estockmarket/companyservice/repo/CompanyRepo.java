90cf9b4ddbc27cdf1c4b3f11bbdc0f7c5e6bf800
Abdus Saboor <gaffari98@gmail.com>
2022-08-27 18:16:04 +0530

    repo refactor

--- backend/company-service/src/main/java/com/estockmarket/companyservice/repo/CompanyRepo.java
+++ backend/company-service/src/main/java/com/estockmarket/companyservice/repo/CompanyRepo.java
@@ -1,9 +1,11 @@
 package com.estockmarket.companyservice.repo;
 
-import com.estockmarket.companyservice.entity.CompanyEntity;
+import com.estockmarket.companyservice.entity.Company;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CompanyRepo extends MongoRepository<CompanyEntity, String> {
+@Repository
+public interface CompanyRepo extends MongoRepository<Company, String> {
     
 }
