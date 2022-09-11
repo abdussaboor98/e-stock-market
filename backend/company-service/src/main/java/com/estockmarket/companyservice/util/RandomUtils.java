c4f364b451dbee03d7dd36b4386b2fa5a6fc8e25
Abdus Saboor <gaffari98@gmail.com>
2022-09-11 21:46:25 +0530

    sonarlint changes

--- backend/company-service/src/main/java/com/estockmarket/companyservice/util/RandomUtils.java
+++ backend/company-service/src/main/java/com/estockmarket/companyservice/util/RandomUtils.java
@@ -4,9 +4,12 @@ import java.util.Random;
 
 public class RandomUtils {
 
+    private static Random random = new Random();
+
     public static char getRandomUpperCaseChar() {
         // ASCII for 'A' is 65 and there are 26 alphabets in total
-        Random random = new Random();
-        return (char) (Math.round(random.nextInt(26)) + 65);
+        return (char) (random.nextInt(26) + 65);
     }
+
+    private RandomUtils(){}
 }
