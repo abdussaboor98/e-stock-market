fc43337daaf684a440adecd73977063d9bd7f044
Abdus Saboor <gaffari98@gmail.com>
2022-09-18 14:17:07 +0530

    stock svc init

---
+++ backend/stock-service/src/main/java/com/estockmarket/stockservice/StockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.estockmarket.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServiceApplication.class, args);
+	}
+
+}
